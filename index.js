b41b77166006d2b304688d10d016c7fdf82d9dc3
MaverickEdwards <r.edwards0917@gmail.com>
2022-11-04 00:13:23 -0400

    commit tree c8986f270dd1c0cb41d0730399b6860240dcd040

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(numOfBlocks) {
+    if (numOfBlocks < 42)
+    return 42 - numOfBlocks
+     else if(numOfBlocks > 42)
+    return numOfBlocks - 42
+  
+}
+function distanceFromHqInFeet(numOfFeet) {
+    return distanceFromHqInBlocks (numOfFeet) * 264
+}
+
+function distanceTravelledInFeet(a, b) {
+  if (a >= 42){
+  return (b - a) * 264}
+  else {
+    return (a - b) * 264
+  }
+}
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) < 400) {
+        return 0;
+    }
+    if (
+        distanceTravelledInFeet(start, destination) > 400 &&
+        distanceTravelledInFeet(start, destination) < 2000
+    ) {
+        return 2.56;
+    }
+    if (
+        distanceTravelledInFeet(start, destination) > 2000 && 
+        distanceTravelledInFeet(start, destination) < 2500
+    ) {
+        return 25;
+    }
+    if (distanceTravelledInFeet(start, destination) > 2500) {
+        return "cannot travel that far";
+}
+}
+        
